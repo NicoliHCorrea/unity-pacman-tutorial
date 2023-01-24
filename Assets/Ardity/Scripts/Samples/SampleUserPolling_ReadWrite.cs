92c699208705748e788ae70a21fe0b87dc491ff1
Nick Correa <nicolicorrea@poli.ufrj.br>
2023-01-23 21:21:03 -0300

    teste read

--- Assets/Ardity/Scripts/Samples/SampleUserPolling_ReadWrite.cs
+++ Assets/Ardity/Scripts/Samples/SampleUserPolling_ReadWrite.cs
@@ -15,40 +15,18 @@ using System.Collections;
 public class SampleUserPolling_ReadWrite : MonoBehaviour
 {
     public SerialController serialController;
+    public int count = 0;
 
     // Initialization
     void Start()
     {
         serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
 
-        Debug.Log("Press A or Z to execute some actions");
     }
 
     // Executed each frame
     void Update()
     {
-        //---------------------------------------------------------------------
-        // Send data
-        //---------------------------------------------------------------------
-
-        // If you press one of these keys send it to the serial device. A
-        // sample serial device that accepts this input is given in the README.
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log("Sending A");
-            serialController.SendSerialMessage("A");
-        }
-
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Debug.Log("Sending Z");
-            serialController.SendSerialMessage("Z");
-        }
-
-
-        //---------------------------------------------------------------------
-        // Receive data
-        //---------------------------------------------------------------------
 
         string message = serialController.ReadSerialMessage();
 
@@ -62,5 +40,9 @@ public class SampleUserPolling_ReadWrite : MonoBehaviour
             Debug.Log("Connection attempt failed or disconnection detected");
         else
             Debug.Log("Message arrived: " + message);
+        if (message == "1")
+        {
+            count += 1;
+        }
     }
 }
